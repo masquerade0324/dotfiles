7ae6955165087767a59ba9160ce034aa0ee240f7
masquerade0324 <masquerade0324@gmail.com>
2018-07-09 23:39:27 +0900

    modify font height: 120 -> 140

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -113,7 +113,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "black" :foreground "#55FF55" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ricty"))))
+ '(default ((t (:stipple nil :background "black" :foreground "#55FF55" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "unknown" :family "Ricty"))))
  '(cursor ((((class color) (background dark)) (:background "#00AA00")) (((class color) (background light)) (:background "#999999")) (t nil))))
 ;; (set-face-attribute 'default nil
 ;;                     :family "Ricty"
